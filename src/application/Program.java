d405f074c0b34b05d63350fb7a7b646968a2ea22
Helio Junior <93434324+HelioDevv@users.noreply.github.com>
2024-03-02 11:55:31 -0300

    Data Recovery - Access

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+
+import db.DB;
 
 public class Program {
 
@@ -9,6 +13,20 @@ public class Program {
 		
 		Connection conn = null;
 		Statement st = null;
+		ResultSet rs = null;
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.createStatement();
+			
+			rs = st.executeQuery("select * from department");
+			
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
 
 	}
 
